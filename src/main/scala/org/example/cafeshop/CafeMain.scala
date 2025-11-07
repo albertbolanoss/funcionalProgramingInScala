011a0f4c71b4c09260bbea1fc127d1eb61305be3
Luis Alberto Bolaños Salazar <68343185+albertbolanoss@users.noreply.github.com>
2025-11-07 08:31:39 -0500

    create coalesced aditional cart

--- src/main/scala/org/example/cafeshop/CafeMain.scala
+++ src/main/scala/org/example/cafeshop/CafeMain.scala
@@ -4,6 +4,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val cafe = new Cafe
     val cc = CreditCard("1234-5678-9012-3456")
+    val cc2 = CreditCard("9999-9999-9999-9999")
 
     val (coffee, charge) = cafe.buyCoffee(cc)
     println(s"Compré un café de precio ${coffee.price}, cargo: ${charge.amount}, use the cc ${charge.cc.number}")
@@ -11,7 +12,7 @@ object Main {
     val (coffees, totalCharge) = cafe.buyCoffees(cc, 3)
     println(s"Compré ${coffees.size} cafés, cargo total: ${totalCharge.amount}, use the cc ${totalCharge.cc.number}")
 
-    val coalesced = cafe.coalesce(List(totalCharge, Charge(cc, 5.0)))
+    val coalesced = cafe.coalesce(List(totalCharge, Charge(cc2, 5.0)))
     println(s"Coalesced charges: $coalesced")
   }
 }
